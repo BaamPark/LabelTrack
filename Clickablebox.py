89daa1591d2e387ba89dc3b18b7589904ac41fb6
BaamPark <baam.park.95@gmail.com>
2023-07-26 18:08:13 -0400

    relocation feature is implemented

--- Clickablebox.py
+++ Clickablebox.py
@@ -4,23 +4,49 @@ from PyQt5.QtWidgets import QLabel
 
 class ClickableImageLabel(QLabel):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent) #https://www.educative.io/answers/what-is-super-in-python
+        #This is separate from the concept of class inheritance. 
+        #When the ClickableImageLabel object is created inside the MainWindow class, MainWindow becomes its parent widget.
         self.parent = parent
         self.start_pos = None
         self.end_pos = None
         self.drawing = False
         self.rectangles = []
         self.clicked_rect = []
+        self.selected_rectangle_index = None
+
+        self.last_pos = None
 
     def mousePressEvent(self, event):
-        if self.parent.btn_add_label.isChecked() and event.button() == Qt.LeftButton:
-            self.start_pos = event.pos()
-            self.drawing = True
+        if event.button() == Qt.LeftButton:
+            for i, rect in enumerate(self.rectangles):
+                if QRect(rect[0], rect[1]).contains(event.pos()):
+                    
+                    self.selected_rectangle_index = i
+                    print("You click box at {}".format(i))
+                    break
+            #for else statement
+            #The “else” block only executes when there is no break in the loop.
+            else:
+                if self.parent.btn_add_label.isChecked():
+                    self.start_pos = event.pos()
+                    self.end_pos = event.pos()  # Also initialize end_pos here
+                    self.drawing = True
+
+            self.last_pos = event.pos()
+
 
     def mouseMoveEvent(self, event):
         if self.drawing:
             self.end_pos = event.pos()
-            self.update()
+   
+        elif self.selected_rectangle_index is not None:
+            offset = event.pos() - self.last_pos
+            start, end = self.rectangles[self.selected_rectangle_index]
+            self.rectangles[self.selected_rectangle_index] = (start + offset, end + offset)
+
+        self.last_pos = event.pos()
+        self.update()
 
     def mouseReleaseEvent(self, event):
         if self.drawing:
